b6ad22f015ca21c82da65264b11c72b30fd60a76
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-08-26 18:57:32 +0545

    clerk:redirect-handler

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
 const DashboardPage = () => {
-  return <p className="text-rose-500">Dashboard Page (protected) </p>;
+  return (
+    <div>
+      <p className="text-rose-500">Dashboard Page (protected) </p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default DashboardPage;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
 const LandingPage = () => {
-  return <div>Landing page (unprotected)</div>;
+  return (
+    <div>
+      Landing page (unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
