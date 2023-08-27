5eaef584bf8f0cdb380ca04290a436283428fdb1
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-08-27 17:37:30 +0545

    update-usebtn,sidebar-basic

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from '@clerk/nextjs';
-
 const DashboardPage = () => {
   return (
     <div>
       <p className="text-rose-500">Dashboard Page (protected) </p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
