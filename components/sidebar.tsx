07c1a7748e2cb3f44002a728213d01040d4aa09d
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-08-27 18:13:48 +0545

    custome-font-without-override-tailwind-with-clsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,14 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { Montserrat } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
+const montserrat = Montserrat({
+  weight: '600',
+  subsets: ['latin'],
+});
 
 const Sidebar = () => {
   return (
@@ -11,6 +19,9 @@ const Sidebar = () => {
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="Logo" src="/logo.png" />
           </div>
+          <h1 className={cn('text-2xl font-bold', montserrat.className)}>
+            Genius
+          </h1>
         </Link>
       </div>
     </div>
